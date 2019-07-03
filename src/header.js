38f213627821972150ae9a5f27668d16b07822cb
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-02 17:57:08 -0700

    init commit

---
+++ src/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    return (
+      <header>
+        <h1>Header</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
